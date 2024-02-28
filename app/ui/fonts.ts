cda9276702494c17dea8776299e4f16c5ffcb09f
Salahuddin3676 <salahuddin3676@gmail.com>
2024-02-28 23:56:37 +0530

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Montserrat } from 'next/font/google';
+export const montserrat = Montserrat({ subsets: ['latin'] });
+
+import { Lusitana } from 'next/font/google';
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400'] });
